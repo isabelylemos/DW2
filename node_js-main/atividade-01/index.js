5bf9fabaa925c0efe44eb1045752feb811fd50a4
isabelylemos <160620081+isabelylemos@users.noreply.github.com>
2024-09-17 22:11:42 -0300

    Add files via upload

--- node_js-main/atividade-01/index.js
+++ node_js-main/atividade-01/index.js
@@ -17,6 +17,9 @@ app.get("/clientes", (req, res) => {
     { nome: "Yasmin Pires", cpf: "132.654.879-11", endereco: "Pacarai, 50" },
     { nome: "Ana Flávia", cpf: "555.444.777-10", endereco: "Av. Barcelona, 41" },
     { nome: "Gustavo Lanna", cpf: "111.452.777-12", endereco: "Carnaúba, 32" },
+    { nome: "Lucomara Lemos", cpf: "221.552.667-51", endereco: "23 de Junho, 30" },
+    { nome: "João Silva", cpf: "331.444.755-12", endereco: "Jatobá, 22" },
+    { nome: "Pedro Henrique", cpf: "333.467.888-13", endereco: "Carnaúba, 32" },
   ];
 
   res.render("clientes", {
@@ -32,6 +35,9 @@ app.get("/produtos", (req, res) => {
     { nome: "Pizza de Calabresa", preco: 35, categoria: "Pequena" },
     { nome: "Pizza de Marguerita", preco: 48, categoria: "Grande" },
     { nome: "Pizza de Portuguesa", preco: 50, categoria: "Grande" },
+    { nome: "Pizza de Romanesca", preco: 40, categoria: "Média" },
+    { nome: "Pizza de Frango com Catupiry", preco: 52, categoria: "Grande" },
+    { nome: "Pizza de Maracatu", preco: 38, categoria: "Média" },
   ];
 
   res.render("produtos", {
@@ -47,7 +53,10 @@ app.get("/pedidos", (req, res) => {
     {numero: 21, valor: 344},
     {numero: 18, valor: 238},
     {numero: 33, valor: 195},
-    {numero: 45, valor: 101}
+    {numero: 45, valor: 101},
+    {numero: 34, valor: 90},
+    {numero: 42, valor: 85},
+    {numero: 12, valor: 72}
   ];
 
   res.render("pedidos", {
